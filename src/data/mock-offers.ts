07d8fcc8511d5a9c9ae86d4166a197db44787abe
Ben Z <btoperation.benz@gmail.com>
2025-08-16 09:06:58 +0000

    lewaspin.com 的 New Register Free K88 New Register Free K60
    lewaspin.com

--- src/data/mock-offers.ts
+++ src/data/mock-offers.ts
@@ -402,8 +402,8 @@ export const offers: CreditOffer[] = [
     termsAndConditions: 'Higher credit limit and rewards points. Annual fee applies.',
     logoUrl: 'https://placehold.co/600x400.png',
     interestRate: '20% p.a.',
-    promoText: 'New Register Free K88',
-    welcomeBonusText: 'Welcome Bonus 200%',
+    promoText: 'New Register Free K60',
+    welcomeBonusText: 'Welcome Bonus 50%',
   },
   {
     id: '29',
@@ -696,3 +696,4 @@ export const offers: CreditOffer[] = [
     
 
     
+
